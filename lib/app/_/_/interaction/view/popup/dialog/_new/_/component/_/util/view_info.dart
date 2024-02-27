ddefa8ac831ebf3f694c29ee1bee57ef345bec8c
june lee <melodysdreamj@gmail.com>
2024-02-27 21:30:34 +0900

    change view_info formet

---
+++ lib/app/_/_/interaction/view/popup/dialog/_new/_/component/_/util/view_info.dart
@@ -0,0 +1,13 @@
+class ViewInfo {
+  /// Please refer to the following guide to share the view.
+  /// https://juneflutter-doc.junes-architecture.lol/ui/share-your-view
+
+  /// Please write down the author's GitHub ID. If there are contributors later on, please add them as well.
+  final List<String> contributors = [];
+
+  /// Please list in detail any packages or extensions that need to be installed to use this view.
+  final String preInstall = '''''';
+
+  /// Please write down the app name or any tags that could categorize it.
+  final List<String> tag = [];
+}
