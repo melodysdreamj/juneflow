e4561830f73d8a3e0962d9450d40effa7850ba0d
june lee <melodysdreamj@gmail.com>
2024-03-16 16:07:39 +0900

    add Module files

---
+++ lib/util/_/initial_app/ready_functions/after_material_app/_new/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyAfterMaterialApp()
+// Future<void> readyForNew(BuildContext context) async {}
---
+++ lib/util/_/initial_app/ready_functions/before_material_app/_new/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForNew(BuildContext context) async {}
