23c928f04c63fee4bb544cb0c1e57aa31ffe3978
june lee <melodysdreamj@gmail.com>
2024-03-18 21:05:33 +0900

    change main import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'util/config/_/global_imports.dart';
 export 'util/config/_/global_imports.dart';
-
-import 'util/_/initial_app/build_app_widget/build_run_app/_.dart';
-export 'util/_/initial_app/build_app_widget/build_run_app/_.dart';
 
 main() {
   return buildApp();
