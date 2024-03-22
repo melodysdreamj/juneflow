10a7fc4a8bb356f97d7d709889ce77d7d1f75f96
june lee <melodysdreamj@gmail.com>
2024-03-22 10:35:12 +0900

    remove url policy

--- lib/util/_/initial_app/ready_functions/before_run_app/_.dart
+++ lib/util/_/initial_app/ready_functions/before_run_app/_.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import '../../../../../main.dart';
-import 'web_url_strategy/none.dart'
-    if (dart.library.html) 'web_url_strategy/_.dart' as url_strategy;
 import 'widgets_binding/_.dart';
 
 Future<void> readyBeforeRunApp() async {
-  url_strategy.readyForWebUrlStrategy();
   await readyForWidgetsBinding();
 
 }
