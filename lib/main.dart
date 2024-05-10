e4f6c474e3da54f6f6705658bb22152cc7f509f3
june lee <melodysdreamj@gmail.com>
2024-05-10 20:00:36 +0900

    `

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,29 @@ export 'util/config/_/global_imports.dart';
 main() {
   return buildApp();
 }
+
+class InitView extends StatelessWidget {
+  const InitView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                const SizedBox(height: 20),
+                Text("New App",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.displaySmall!.copyWith(fontWeight: FontWeight.bold)),
+                const SizedBox(height: 20),
+                basicButton(context, "New Button", () {}),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
