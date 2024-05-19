3c4be94f3f171885cfc7a00d52a4b333c75066a1
june lee <melodysdreamj@gmail.com>
2024-05-20 06:47:33 +0900

    `

---
+++ lib/template/_new.dialog/dialog.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+import '../../main.dart';
+import '_/_.dart';
+
+void NewDialog(BuildContext context) async {
+  showDialog(
+      context: context,
+      barrierDismissible: true, // dialog is dismissible with a tap on the barrier
+      barrierColor: Colors.black.withOpacity(0.4), // screen color outside the dialog
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.white, // dialog background color
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: NewView(),
+        );
+      });
+}
