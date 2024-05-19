82ff4e4c848fa931711eb93492c4e0481294eacc
june lee <melodysdreamj@gmail.com>
2024-05-19 15:01:41 +0900

    Update _.dart

--- lib/util/_/initial_app/build_app_widget/build_run_app/_.dart
+++ lib/util/_/initial_app/build_app_widget/build_run_app/_.dart
@@ -3,9 +3,14 @@ import '../../../../../main.dart';
 import '../../ready_functions/before_run_app/_.dart';
 import '../build_my_app/_.dart';
 
+
 Future<void> buildApp() async {
   await readyBeforeRunApp();
   Widget childWidget = MyApp();
 
+
+
   return runApp(childWidget);
 }
+
+
