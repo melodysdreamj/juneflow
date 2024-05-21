73da9194dfcc5a25efa89a69ad7cb2a8fe82ee0f
june lee <melodysdreamj@gmail.com>
2024-05-21 20:48:12 +0900

    `

--- lib/util/config/global_imports.dart
+++ lib/util/config/global_imports.dart
@@ -6,7 +6,7 @@ export '../_/initial_app/build_app_widget/build_run_app/_.dart';
 
 export 'package:june_flow_util/june_flow_util.dart';
 
-export 'package:styled_widget/styled_widget.dart';
+export 'package:styled_widget/styled_widget.dart'; //@add
 
 export 'package:gap/gap.dart'; //@add
 
